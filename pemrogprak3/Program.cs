1d021eeb7cf1338cc92d9beae8c8874c5be9f4ed
Haitsama <haitsam@students.amikom.ac.id>
2023-03-26 21:28:01 +0700

    praktikum3

---
+++ pemrogprak3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemrogprak3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            mobil mobil1 = new mobil();
+
+            mobil1.Warna = "Merah";
+            mobil1.JumlahPintu = 4;
+            mobil1.Merk = "Toyota";
+            mobil1.Model = "Avanza";
+            mobil1.TahunKeluar = 2021;
+
+            mobil1.Gas(80);
+            mobil1.Klakson("tottototttotttoootott");
+            mobil1.tampilkaninfo();
+
+            mobil mobil2 = new mobil();
+
+            mobil2.Warna = "Hitam";
+            mobil2.JumlahPintu = 2;
+            mobil2.Merk = "Lamborghini";
+            mobil2.Model = "aventador";
+            mobil2.TahunKeluar = 2018;
+                 
+            mobil2.Gas(300);
+            mobil2.Klakson("wiuwiwuwiwu");
+            mobil2.tampilkaninfo();
+
+
+            Console.Write("Tekan <ESC> untuk keluar");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+        }
+    }
+}
---
+++ pemrogprak3/mobil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemrogprak3
+{
+    internal class mobil
+    {
+        //propertis
+        public string Warna { get; set; }
+        public int JumlahPintu { get; set; }
+        public string Merk { get; set; }
+        public string Model { get; set; }
+        public int TahunKeluar { get; set; }
+
+        //method
+        public void Gas(int kecepatan)
+        {
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1} KM/H", Model, kecepatan);
+        }
+
+        public void Klakson(string suara)
+        {
+            Console.WriteLine("output klakson: {0}\n",suara);
+        }
+
+        public void tampilkaninfo()  
+        {
+            Console.WriteLine("Mobil saya berwarna {0},merek {1} model {2} keluaran tahun {3} dengan jumlah pintu sebanyak {4}\n",Warna,Merk,Model,TahunKeluar,JumlahPintu);
+        }
+
+    }
+}
